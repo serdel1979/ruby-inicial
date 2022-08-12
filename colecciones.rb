74efbbc5223b4e5f98ed1df6e44d6fccf0cfd1d0
Sergio De Luca <sdlbsso@gmail.com>
2022-08-11 23:54:56 -0300

    inicio con ruby

---
+++ colecciones.rb
@@ -0,0 +1,17 @@
+#hay 2 tipos, array y dict
+a = [1,2,3,"hola"]
+p a.length
+p a[5]
+a << 1 << 50
+p a.length
+p a[5]
+a[20] = "ffff"
+b = [1,2,3,5]
+b[2] = [1,2]
+b.push "hola"
+puts "arreglo b ="
+puts b
+b.pop
+p b 
+b.shift
+p b
